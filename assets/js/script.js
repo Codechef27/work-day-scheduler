15a1cc26d829ffb506774d7d5f1a0e538dab7801
BradleyK <bradleyjkennett@gmail.com>
2022-05-17 21:44:45 -0400

    timeblock rows added

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,27 +6,42 @@
 };
 currentDayEl();
 
-// var workHours = [
-//     moment().hour(9).format('ha'),
-//     moment().hour(10).format('ha'),
-//     moment().hour(11).format('ha'),
-//     moment().hour(12).format('ha'),
-//     moment().hour(13).format('ha'),
-//     moment().hour(14).format('ha'), 
-//     moment().hour(15).format('ha'),
-//     moment().hour(16).format('ha'),
-//     moment().hour(17).format('ha')
-// ];
+var workHours = [
+    moment().hour(9).format ('h a'),
+    moment().hour(10).format('h a'),
+    moment().hour(11).format('h a'),
+    moment().hour(12).format('h a'),
+    moment().hour(13).format('h a'),
+    moment().hour(14).format('h a'), 
+    moment().hour(15).format('h a'),
+    moment().hour(16).format('h a'),
+    moment().hour(17).format('h a')
+];
+//build content elements
+for (var i = 0; i < workHours.length; i++){
+//append div container with 9 rows    
+    var container = $(".container")
+    var timeBlockEl = $("<div>")
+    .addClass("row time-block")
+    .attr({id: "row" + ( i + 9)})
+    $(".container").append(timeBlockEl);
+//append time and connected workHours
+    var hourEl = $("<div>")
+    .addClass("col-1 hour")
+    .text(workHours[i])
+    .attr({id: i+9 })
+    $(timeBlockEl).append(hourEl);
+// append textarea space
+    var inputEl = $("<textarea>")
+    .addClass("col-10 textarea")
+    .attr({id:"time-block" + (i + 9)})
+    $(timeBlockEl).append(inputEl);
+// append icon and savebtn
+    var saveBtn = $("<button>")
+    .addClass("col-1 saveBtn")
+    .html("<i class='fas fa-save'></i>").addClass("fa-2x")
+    .attr({id: (i + 9), type: "button"})
+    .on("click", function(){})
+    $(timeBlockEl).append(saveBtn);  
+}
 
-// // create time blocks
-// var container = $(".container");
-// var timeBlockEl = $("<div>").addClass("row time-block");
-// var hourEl = $("<div>").addClass("col-1 hour");
-// var inputEl = $("<textarea>").addClass("col-10");
-// var saveBtn = $("<button>").addClass("col-1 saveBtn").append(<i class="fa-solid fa-floppy-disk-circle-arrow-right"></i>);
-
-
-// for (var i = 0; i < workHours.length; i++){
-//    container.append(timeBlockEl.attr({id: "row"(i + 9)}).add(hourEl));
-
-// }
