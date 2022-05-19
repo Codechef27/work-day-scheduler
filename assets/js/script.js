748aac4b994bdc60a2493df6890eaa2c662a19de
BradleyK <bradleyjkennett@gmail.com>
2022-05-19 12:22:39 -0400

    task persist added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,4 @@
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-// WHEN I click into a time block
-// THEN I can enter an event
-// WHEN I click the save button for that time block
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
+
 // clock and date--------------------------------------------->
  var currentDayEl = function() {
     var currentDay = moment ();
@@ -47,10 +35,9 @@ for (var i = 0; i < workHours.length; i++){
     var inputEl = $("<textarea>")
     .addClass("col-10 textarea")
     .attr({id:(i + 9) + "time-block"})
+    .text('');
     $(timeBlockEl).append(inputEl);
-    // auditTimeBlock(inputEl);
-   
-// append icon and savebtn----------------------->
+//append the button
     var saveBtn = $("<button>")
     .addClass("col-1 saveBtn")
     .html("<i class='fas fa-save'></i>").addClass("fa-2x text-warning")
@@ -82,18 +69,30 @@ $('.textarea').each(function () {
 
   }
 })
-// if(localStorage.getItem(i)){
-//   document.getElementById(i).value = localStorage.getItem(i);
-// }
+
 // //saveTasks--------------------------------------------------------->
 function saveTask(){
   $(".saveBtn").on("click",function() {
-    var hour = $(this).siblings(".textarea").val();
-    var task = $(this).parent().attr("id"); 
+    var task = $(this).siblings(".textarea").val();
+    var hour = $(this).parent().attr("id"); 
     localStorage.setItem(hour, task);
   })
+
 }
+// persist task on refresh
+function loadTask (){
+  
+  $("#9row .textarea").val(localStorage.getItem("9row"));
+  $("#10row .textarea").val(localStorage.getItem("10row"));
+  $("#11row .textarea").val(localStorage.getItem("11row"));
+  $("#12row .textarea").val(localStorage.getItem("12row"));
+  $("#13row .textarea").val(localStorage.getItem("13row"));
+  $("#14row .textarea").val(localStorage.getItem("14row"));
+  $("#15row .textarea").val(localStorage.getItem("15row"));
+  $("#16row .textarea").val(localStorage.getItem("16row"));
+  $("#17row .textarea").val(localStorage.getItem("17row"));
 
+};
 
-
+loadTask();
 saveTask();
